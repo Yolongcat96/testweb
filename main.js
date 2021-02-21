2a23972a321fa344ff74c9e305ec0f2b36d611af
Sunmi Seol <smseol@stanford.edu>
2021-02-21 11:03:12 -0800

    created web frame

--- main.js
+++ main.js
@@ -1,5 +1,65 @@
 'use strict';
 
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(navbarHeight);
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click',() => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// show "arrow-up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight/2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle scrolling when tapping "arrow-up" button
+const arrowUpBtn = document.querySelector('.arrow-up');
+arrowUpBtn.addEventListener('click', () => {
+    scrollIntoView('#home');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});
+}
+
+// Image sliders
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +86,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
